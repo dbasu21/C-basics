ea8743c3ee806770e3ebe6189004c5225fb96b85
dbasu21 <97442465+dbasu21@users.noreply.github.com>
2022-07-29 16:28:04 +0530

    Add files via upload

---
+++ 29-07 .c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() 
+{
+	/*float areaofcircle;
+	float radius;
+	int ourage;
+    int number = 25;
+    char star = '@';
+    float pi = 3.16;
+    // hello
+     long
+    hello*/
+    /*
+    int a =30;
+    int A=40;
+    printf("Kuch vhi\n");
+    printf("khabhi khusi khabhi gam");
+    printf("pi is %f \n",pi);
+    printf("number is %d \n",number);
+    printf("star is %c \n",star);
+    printf("enter  urage\n");
+    scanf("%d",&urage);
+    printf("enter  myage\n");
+    scanf("%d",&myage);
+     
+    ourage = myage +urage; 
+    printf("ourage is %d\n",ourage );
+    int _a = 45;
+    int final_price = 100;
+    printf("enter the radius\n");
+    scanf("%f",&radius);
+    areaofcircle=radius*radius*(22/7);
+    printf("area of square is %f ",areaofcircle);
+    */
+    int d,e,f;
+    d=e=f=67;
+    printf("%d",e);
+    return 0;
+}
